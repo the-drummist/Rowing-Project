a841456ad5cc873d41d063dfbe5bbb3e17277293
the-drummist <adkramer2003@gmail.com>
2021-02-11 13:29:06 -0500

    Added session constructor
    added session constructor to run.py and updated the requirements to reflect the changes

--- website/run.py
+++ website/run.py
@@ -1,8 +1,9 @@
 from app import create_app
 from flask_pwa import PWA
-
+from flask_session import Session
 app = create_app()
 
+Session(app)
 PWA(app)
 
 app.run(debug=True, host="0.0.0.0", port="5000")
