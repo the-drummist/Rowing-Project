462076d0b549e14105bb55fb7263b002f22f59ca
the-drummist <adkramer2003@gmail.com>
2021-03-04 11:40:00 -0500

    Created a file for helper functions for the backend

---
+++ website/app/helper.py
@@ -0,0 +1,18 @@
+from functools import wraps
+from flask import url_for, redirect
+def login_required(f):
+	"""
+	Decorate routes to require login.
+
+	http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
+	"""
+	@wraps(f)
+	def decorated_function(*args, **kwargs):
+		if session.get('user_id') is None:
+			return redirect(url_for('login'))
+		return f(*args, **kwargs)
+	return decorated_function
+
+def generate_key():
+
+def validate_key():
