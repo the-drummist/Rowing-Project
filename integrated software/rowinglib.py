608c581168475e20d3b7b072e3cc5bb12ee0ae85
the-drummist <adkramer2003@gmail.com>
2021-03-04 11:39:36 -0500

    Created a better layout for the future of the class

--- integrated software/rowinglib.py
+++ integrated software/rowinglib.py
@@ -1,7 +1,27 @@
 # helper library for rowinguard
+from datetime import datetime
 
-class StoreData:
-	
-	def __init__(self, file):
-		
-		self.file = file
+class Rowinguard:
+
+	def __init__(self):
+		self.emg_file = generate_name('emg')
+		self.vitals_file = generate_name('vitals')
+
+	def generate_name(dataform):
+		if dataform == 'emg' or dataform == 'vitals':
+			date = datetime.now().strftime('_%Y_%m_%d-%I_%M_%S_%p')
+			filename = dataform + date + '.csv'
+			return filename
+
+
+	def store_data(self):
+		pass
+
+	def start_workout(self):
+		pass
+
+	def end_workout(self):
+		pass
+
+
+
