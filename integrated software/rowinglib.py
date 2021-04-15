42fe735d56c0cfe489fb106fec9f3043627a2a00
the-drummist <adkramer2003@gmail.com>
2021-04-15 08:01:53 -0400

    Update rowinglib.py
    helper class for rowinguard

--- integrated software/rowinglib.py
+++ integrated software/rowinglib.py
@@ -1,27 +1,50 @@
 # helper library for rowinguard
 from datetime import datetime
+from multiprocessing import Process
+import RPi.GPIO as GPIO
 
 class Rowinguard:
 
-	def __init__(self):
+	def __init__(self, interupt):
 		self.emg_file = self.generate_name('emg')
 		self.vitals_file = self.generate_name('vitals')
+		self.interupt = interupt
+		GPIO.setmode(GPIO.BOARD)
+
 
 	def generate_name(self, dataform):
 		if dataform == 'emg' or dataform == 'vitals':
-			date = datetime.now().strftime('_%Y_%m_%d-%I_%M_%S_%p')
-			filename = dataform + date + '.csv'
+			date = datetime.now().strftime('%Y_%m_%d-%I_%M_%S_%p_')
+			filename = date + dataform + '.csv'
 			return filename
 
+	def start_peripherals(self, *funcs):
+	toJoin = list()
+	# create a process for each target function and start it
+	for func in funcs:
+		process = Process(target=func)
+		process.start()
+		toJoin.append(process)
+	# join the processes
+	for process in toJoin:
+		process.join()
+	return toJoin
 
-	def store_data(self):
-		pass
+
+	def alert(self, type):
+
 
 	def start_workout(self):
-		pass
+		# wait for interupt here
+		GPIO.wait_for_edge(self.interupt, GPIO.RISING)
+		emg = EMG(0)
+		vitals = MAX30102(3)
+		return emg, vitals
 
 	def end_workout(self):
-		pass
+		# wait for interupt
+		GPIO.wait_for_edge(self.interupt, GPIO.FALLING)
+		
 
 
 
